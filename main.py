2fa2a02e27b0db49c2eecadc4f8da0beed072ec7
gimsinu067 <gimsinu067@gmail.com>
2025-08-08 14:46:51 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+payload = {
+    "provider": None,
+    "voip_provider": None
+}
+
+headers = {
+    "accept": "*/*",
+    "accept-language": "ko",
+    "authorization": "YOUR_DISCORD_TOKEN_HERE",
+    "content-type": "application/json",
+}
+
+response = requests.post("https://discord.com/api/v9/auth/logout", headers=headers, json=payload)
+
+if response.status_code == 204:
+    print(response.json())
